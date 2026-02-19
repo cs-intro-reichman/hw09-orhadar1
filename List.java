3b82c6ca4fddb719d51e6b5819a94b15570f256d
orhadar1 <or.hadar01@post.runi.ac.il>
2026-02-19 21:51:03 +0200

    Fix typo in comment for iterator method

--- List.java
+++ List.java
@@ -93,7 +93,7 @@ public class List {
             current = current.next;
             i++;
         }
-        // Returns an iterator that starts in that elementt
+        // Returns an iterator that starts in that element
 	    return new ListIterator(current);
     }
 }
